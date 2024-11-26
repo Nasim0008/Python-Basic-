d4e5458fa0869aad588896f78178c7f1435628f2
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-11-26 13:28:01 +0600

    string literal

---
+++ Comment/string_literal.py
@@ -0,0 +1,11 @@
+# string literal:
+"""
+string literal is a special type of string which is not store in the variable.
+python ignore the string literal. And string literal use as a python comments
+
+"""
+
+"My name is Md Nasim Hossen" # this is string literal that is an ignore by the compiler and use as a single line comment
+'My Country name is Bangladesh' # this is also string literal and an ignore by the compiler and use a single line comment
+"""My Favorite person in this world is my Father""" # this is also string literal an ignore by the compiler and use a single line comment
+print("Md Nasim Hossen")
