de82178db6871761e05f42c1563664ff9e59a493
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-11-26 12:52:34 +0600

    multiline comment

---
+++ Comment/Multiline_comment.py
@@ -0,0 +1,14 @@
+# Multiline Comment:
+
+"""
+This is the multiline comment
+we can write here multiple line of documentation for the code
+multiline comment use for the big documentation
+3 double quotation will use for the multiline comment
+"""
+
+
+"""
+a = 10
+b = 2
+print(a,b)"""
