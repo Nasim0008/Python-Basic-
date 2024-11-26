0cd5ad34f2638f185cef1c9e1a0dbccc171aa7c5
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-11-26 12:45:28 +0600

    single and multiline comment

---
+++ Comment/comment.py
@@ -0,0 +1,17 @@
+# single line comment:
+a = 10
+#print(a)
+#print("Md Nasim Hossen")
+
+print("Md Nasim Hossen") # This is a comment
+
+
+# Multiline Comment:
+
+"""
+This is the multiline comment 
+we can write here multiple line of documentation for the code
+multiline comment use for the big documentation
+3 double quotation will use for the multiline comment
+"""
+
