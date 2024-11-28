c1f6c20887ba45896703447c3cbbbf4eecc519df
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-11-28 22:21:20 +0600

    find the type of the variable

---
+++ Variable/find_type.py
@@ -0,0 +1,15 @@
+a = 10 # int data type
+print(type(a))
+
+
+b = 10.02222 # float data type
+print(type(b))
+
+
+ch = "a" #there is no character data type in python.it is a string which size is 1
+print(type(ch))
+
+name = "Md Nasim Hossen" #string data type
+print(type(name))
+
+
