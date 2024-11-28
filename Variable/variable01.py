21e51a5e0b9546d76c0f9765b582ef255370b6b4
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-11-28 21:12:03 +0600

    variable declearation

---
+++ Variable/variable01.py
@@ -0,0 +1,8 @@
+a = 10 # int data tpye
+b = 10.02222 # float data type
+ch = "a" #there is no character data type in python.it is a string which size is 1
+name = "Md Nasim Hossen" #string data type
+
+print(a,b,ch,name)
+
+
