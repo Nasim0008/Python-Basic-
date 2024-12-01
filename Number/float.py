12d019604c776250f41fa3387fd66bab4254e526
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-01 15:00:59 +0600

    normal floating point number

---
+++ Number/float.py
@@ -0,0 +1,14 @@
+a = 10.5 # positive and containing one decimal point
+print(type(a),a)
+
+
+b = 10.23990 # positive and containing more than 1 decimal point
+print(type(b),b)
+
+
+c = -10.4 # Negative and containing 1 decimal point
+print(type(c),c)
+
+
+d = -10.485030 #Negative and contain more than 1 decimal point
+print(type(d),d)
