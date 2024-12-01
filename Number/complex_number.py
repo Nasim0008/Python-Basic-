963d68f75ea78fe4334e28638205a469e46aa1cf
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-01 15:29:43 +0600

    complex number

---
+++ Number/complex_number.py
@@ -0,0 +1,11 @@
+# j is the imaginary part
+
+x = 3+5j # real part and imaginary part
+print(type(x),x) # (3+5j)
+
+y = 5j # only positive imaginary part
+print(type(y),y)
+
+z = -5j #only negative imaginary part
+print(type(z),z)
+
