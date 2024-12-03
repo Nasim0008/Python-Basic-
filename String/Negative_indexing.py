9cb3ef0faa8661202db3a0862a5ad9a17892d374
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-03 17:12:27 +0600

    Negative indexing in string

---
+++ String/Negative_indexing.py
@@ -0,0 +1,12 @@
+
+
+"""
+In python, there are two types of indexing of the string.
+ 1. Normal indexing: string from the 0th index to the (n-1)ith index
+ 2. Negative indexing : string from the (-n) to the (-1)
+"""
+
+s = "Nasim"
+print(s[-1],s[-2],s[-3],s[-4],s[-5]) # Negative indexing print the string from the end to start
+
+print(s[-4:-2])
