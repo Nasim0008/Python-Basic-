673b2fd30e80f957ebf2290f07c270a1ca2be75f
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-03 14:01:17 +0600

    multiline text store in a single variable and print it

---
+++ String/multiline_string.py
@@ -0,0 +1,5 @@
+a = """ My Name is Md Nasim Hossen. Currently I am learning python for work as backend developer.
+when i completely learn basic python. then i will learn git and github and then i will learn MySQL then i have to learn
+Django. Then i have to work with the python project.
+"""
+print(a)
