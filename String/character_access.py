3f220a56034a158b6c39a01e1080f4ccfa9b7fba
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-03 16:36:49 +0600

    indexing of the string

---
+++ String/character_access.py
@@ -0,0 +1,8 @@
+
+"""
+string is the array of character.
+There is no character data type in python.
+A single character is a string which size is 1
+"""
+s= "Md Nasim Hossen"
+print(s[0],s[4],s[7])
