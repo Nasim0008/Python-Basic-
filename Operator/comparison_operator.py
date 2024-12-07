acd68fe352764f2e4ba1f360badc37cf4f6b0caa
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-07 13:46:46 +0600

    comparison operator for comparing two or multiple value

---
+++ Operator/comparison_operator.py
@@ -0,0 +1,25 @@
+# equal operator (==)
+print(8==8) # True
+print(5==9) # False
+
+# Not equal operator (!=)
+print(4!=100) # True
+print(5!=5) # False
+
+# greater than operator (>)
+print(5>3) #True
+print(20>500) # False
+
+# greater than or equal to operator (>=)
+print(38>=2) # True
+print(20>=20) # True
+print(10>=200) # False
+
+# less than operator (<)
+print(10<100) # True
+print(10<3) # False
+
+# less than or equal to operator (<=)
+print(10<=200) # True
+print(100<=100) # True
+print(200<=20) # False
