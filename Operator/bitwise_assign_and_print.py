0ab020ca5418a9918ef09a229bf90566b5e3d957
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-07 14:00:31 +0600

    bitwise operation and assignment together

---
+++ Operator/bitwise_assign_and_print.py
@@ -0,0 +1,30 @@
+# OR operator (|)
+a = 20
+b= 3
+a|=b
+print(a)
+
+# AND operator (&)
+a = 20
+b = 5
+a&=b
+print(a)
+
+# XOR operator (^)
+a = 20
+b = 5
+a^=b
+print(a)
+
+# left shift operator (<<)
+a = 20
+b = 4
+a<<=b
+print(a)
+
+# right shift operator (>>)
+a =20
+b = 2
+a>>=b
+print(a)
+
