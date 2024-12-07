b33dcfa1c4a7035800f255dec9d667e9af33b682
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-07 13:16:50 +0600

    all arithmetic operation

---
+++ Operator/arithmatic_operator.py
@@ -0,0 +1,36 @@
+#addition
+a , b = 10,3
+c  = a+b
+print(c,a+b)
+
+#subtraction
+a ,b = 10,3
+c = a-b
+print(c,a-b)
+
+#multiplication
+a, b = 10,3
+c = a*b
+print(c, a*b)
+
+# decimal division
+a,b = 10,4
+c = a/b
+print(c,a/b)
+
+# integer division
+a,b = 10,4
+c = a//b
+print(c,a//b)
+
+# Modulus
+a,b = 10,3
+c = a%b
+print(c,a%b)
+
+#exponentiation
+base,power = 5,4
+c = base**power
+print(c, base**power)
+
+
