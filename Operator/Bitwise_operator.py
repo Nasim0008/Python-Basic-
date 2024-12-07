1f0b1e0a11f01f9529ae08dda48280a26ce293ea
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-07 13:56:06 +0600

    all bitwise operation

---
+++ Operator/Bitwise_operator.py
@@ -0,0 +1,35 @@
+# OR operator (|)
+a = 20
+b= 3
+c = a|b
+print(c, a|b)
+
+# AND operator (&)
+a = 20
+b = 5
+c = a&b
+print(c, a&b)
+
+# XOR operator (^)
+a = 20
+b = 5
+c = a^b
+print(c, a^b)
+
+# NOT operator (~)
+a = 20
+c = ~a
+print(c, ~a)
+
+# left shift operator (<<)
+a = 20
+b = 4
+c = a<<b
+print(c, a<<b)
+
+# right shift operator (>>)
+a =20
+b = 2
+c = a>>b
+print(c,a>>b)
+
