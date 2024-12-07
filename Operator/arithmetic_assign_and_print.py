7894f0399e78af132b6786124b0b3d35d09f839b
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-07 13:28:17 +0600

    arithmetic operation and assignment together

---
+++ Operator/arithmetic_assign_and_print.py
@@ -0,0 +1,36 @@
+#addition assignment and printing
+a , b = 10,3
+a+=b
+print(a)
+
+#subtraction assignment and printing
+a ,b = 10,3
+a-=b
+print(a)
+
+#multiplication assignment and printing
+a, b = 10,3
+a*=b
+print(a)
+
+# decimal division assignment and printing
+a,b = 10,4
+a/=b
+print(a)
+
+# integer division assignment and printing
+a,b = 10,4
+a//=b
+print(a)
+
+# Modulus assignment and printing
+a,b = 10,3
+a%=b
+print(a)
+
+#exponentiation
+base,power = 5,4
+base**=power
+print(base)
+
+
