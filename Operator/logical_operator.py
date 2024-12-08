e95ef24b42e3644bc88272b49834af3eb897db0f
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-08 14:59:07 +0600

    all logical operator (and, or , not)

---
+++ Operator/logical_operator.py
@@ -0,0 +1,31 @@
+"""
+logical operators are used to combine the conditional statements
+"""
+
+s = "Nasim"
+x = 10
+
+# and operator (and)
+if s=="Nasim" and x ==10: # Return True if all the statement is True
+    print("YES")
+else:
+    print("NO")
+
+# or operator (or)
+if s=="Nasim" or x ==1: # Return True if one of the statement is True
+    print("YES")
+else:
+    print("NO")
+
+
+# not operator (not)
+flag=False
+if not flag: # Reverse the result
+    print("YES")
+else:
+    print("NO")
+
+
+
+
+
