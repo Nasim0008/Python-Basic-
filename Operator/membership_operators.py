723184bc78a5ab9178431ea17011125d98326f00
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-08 15:25:18 +0600

    membership operator (in, not in)

---
+++ Operator/membership_operators.py
@@ -0,0 +1,9 @@
+# in operator (in)
+s = "Md Nasim Hossen"
+if "Nasim" in s: # Return True if a specified value is present in the object
+    print("YES")
+
+# not in operator (not in)
+s = "Md Nasim Hossen"
+if "Bangla" not in s: # Return True if a specified value is not present in the object
+    print("YES")
