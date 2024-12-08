7c23d297dde15eb796896569a87938b48b31165a
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-08 23:15:50 +0600

    center the string

---
+++ String/String_Method/center.py
@@ -0,0 +1,10 @@
+"""
+The center methode will center align the string, using specified character (by default space) as fill the character
+"""
+
+s = "Nasim"
+s1 = s.center(30) # centralize the string (by default white space)
+print(s1)
+
+s2 = s.center(30,"*") # centralize the string by using "*" character
+print(s2)
