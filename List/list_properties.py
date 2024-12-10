5883a1e0f8f180c82a7ddc04f2cc0fef70e30bec
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-10 17:27:32 +0600

    Properties of list ( ordered, changeable, allow duplicate)

---
+++ List/list_properties.py
@@ -0,0 +1,25 @@
+# ordered:
+"""
+list are ordered, it means that each and every element has specific index
+and we can access that index
+"""
+
+li = [1,2,3,4,5,6]
+print(li[0])
+print(li[3])
+print(li)
+
+# Changeable:
+"""
+we can modify the list (add, remove,clear) by using list methode
+"""
+
+
+# allowed Duplicated
+"""
+list allow duplicate elements but each elements has different index
+"""
+
+li = ["Nasim","Hossen","bangla","English","Nasim"]
+print(li[0],li[4])
+print(li)
