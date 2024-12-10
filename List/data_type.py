192231d2371906ef2d21f7f08fbfd279d28318ca
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-10 17:38:09 +0600

    all types of data for the list

---
+++ List/data_type.py
@@ -0,0 +1,19 @@
+#int
+li_int = [1,2,3,4,5,6]
+print(li_int)
+
+# float
+li_float = [1.2,2.5,5.9,7.2,85.2]
+print(li_float)
+
+# string
+li_string = ["Md", "Nasim", "hossen","Bangla","englis"]
+print(li_string)
+
+#boolean
+li_bool = [True,True,False,True,False]
+print(li_bool)
+
+#different data types of list
+diff_li = [10,20,30.433,503.21,"Nasim","Bangla",False,True]
+print(diff_li)
