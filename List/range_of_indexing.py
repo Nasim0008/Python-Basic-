5fec161b597a8dee0f38670d6e9c47d1404967dc
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-11 13:11:13 +0600

    range of indexing of the list

---
+++ List/range_of_indexing.py
@@ -0,0 +1,9 @@
+li = ["Md","Nasim","Bangla","English","Math","pen","Mobile","calculator","Glass"]
+
+print(li[2:6]) # print from 2nd index element to the 5th index element (6th is excluded)
+
+print(li[:7]) # print from 0th index element to the 6th index element (7th is excluded)
+
+print(li[5:]) # print from 5th index element (5th is included) to the last index element
+
+
