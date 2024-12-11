7212e98d4677ed6dee42e2b1587a03a7709e742a
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-11 13:22:15 +0600

    Range of negative indexing of the list

---
+++ List/range_of_negative_indexing.py
@@ -0,0 +1,21 @@
+li  =  ["Md","Nasim","Bangla","English","Math","pen","Mobile","calculator","Glass"]
+#index:  -9 , -8   , -7    ,   -6    , -5    , -4  ,  -3   ,    -2      ,  -1
+
+n = len(li)
+
+"""
+In negative indexing, 
+first element index = -n
+Second element index = (-n)+1
+Third element index = (-n)+1+1
+.........................
+.........................
+.........................
+last element index = -1
+"""
+
+print(li[-7:-2])
+
+print(li[:-3])
+
+print(li[-6:])
