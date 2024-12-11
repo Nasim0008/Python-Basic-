7c2d61b1cc2fd6f5fd869706f491d2a8be0a5519
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-11 13:01:58 +0600

    Negative indexing

---
+++ List/Negative_index.py
@@ -0,0 +1,12 @@
+
+"""
+Negative indexing means starting from the end
+-1 means last element, -2 means second last elements etc
+"""
+
+li = [1,2,3,4,5,6]
+
+print(li[-1])
+print(li[-2])
+n = len(li) # size of the list
+print(li[-n])
