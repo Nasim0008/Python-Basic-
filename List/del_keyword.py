273c8649883c0f7867c7ecd685b6d47e93c9cfca
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-12 14:15:46 +0600

    delete specified item and completely delete the list using del keyword

---
+++ List/del_keyword.py
@@ -0,0 +1,8 @@
+li = ["one","two","three","four","five"]
+print(li)
+del li[0] # delete the 1st index item
+print(li)
+
+del li # completely delete the list from the code. we have to again create that list to use
+
+#print(len(li))
