0fb5b554928088371e200843545bc0d472fc8454
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-12 14:06:05 +0600

    remove the specified item from the list

---
+++ List/remove_specified_item.py
@@ -0,0 +1,7 @@
+li = ["one","two","three","four"]
+li.remove("three") # remove method remove the specified item
+print(li)
+
+li = ["one","two","three","two","one","two"]
+li.remove("two") # if there are more than one specified item, remove function remove the first occurrence
+print(li)
