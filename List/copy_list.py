bd22f662b019148abaf4143502e4512b95264663
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-14 11:56:32 +0600

    copy the list

---
+++ List/copy_list.py
@@ -0,0 +1,9 @@
+li = [1,2,3,4,5,6,7]
+
+li_new=li.copy() # copy the list
+print(li_new)
+
+li_new = list(li) # another way to copy the list
+print(li_new)
+
+li_new = list(li[:]) # another way to copy the list
