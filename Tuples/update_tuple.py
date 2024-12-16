d5570310ec0364a618f1df748b85c6c403821950
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-16 16:13:40 +0600

    add, remove, del, insert, modify all the operation of the tuple

---
+++ Tuples/update_tuple.py
@@ -0,0 +1,9 @@
+"""
+tuple are unchangeable, we can not add, delete, insert, modify of the tuple
+so , we can convert the tuple to the list then after the update we can again can convert the tuple
+"""
+
+tp = (1,2,3,4,5,6)
+li = list(tp) # tuple to the list
+# we can modify the list as wish we can
+tp = tuple(li) # tuple to the list
