57f17854046f7dd1fed7061e2467620446d0070e
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-16 15:29:07 +0600

    all the properties of tuples

---
+++ Tuples/properties.py
@@ -0,0 +1,21 @@
+tp=(1,2,3,4,5,6)
+
+# tuple items are ordered, unchangeable,allow duplicate value
+
+# Ordered:
+"""
+Tuples item are ordered, it means that all the tuples item are order,and this order will not changed
+"""
+print(tp[0],tp[3])
+
+#Unchangeable:
+#Tuples are unchangeable, meaning that we cannot change, add or remove items after the tuple has been created.
+
+
+# Allow Duplicates:
+"""
+Since tuples are indexed, they can have items with the same value
+"""
+
+tp = ("a","b","c","a","b")
+print(tp[0],tp[3])
