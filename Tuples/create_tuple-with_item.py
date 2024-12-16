28868f630f4a33f071b91835a36cd671efa29dd3
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-16 15:38:33 +0600

    1 item tuples creation

---
+++ Tuples/create_tuple-with_item.py
@@ -0,0 +1,18 @@
+"""
+To create a tuple with one item, we have to add comma after the item,
+otherwise python does not recognise it as tuples
+"""
+
+
+
+tp = (1,) #tuple
+print(tp,type(tp))
+
+tp=(1) # int
+print(tp,type(tp))
+
+tp=("Nasim",) #tuple
+print(tp,type(tp))
+
+tp = ("Nasim") #string
+print(tp,type(tp))
