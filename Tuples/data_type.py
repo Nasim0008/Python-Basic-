6d99ab2fb82aecd42a88e84ea2b2407aea59a70a
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-16 15:43:42 +0600

    data type of tuples

---
+++ Tuples/data_type.py
@@ -0,0 +1,15 @@
+tp = (1,2,3,4,5,6) # tuple with int values
+print(tp)
+print(type(tp))
+
+tp = ("a","b","c","d") # tuple with string values
+print(tp)
+print(type(tp))
+
+tp = (False,True,False,True,True) # tuple with boolean values
+print(tp)
+print(type(tp))
+
+tp = (1,2,3,"a","b","c",True, False) # tuple with int, string, boolean values
+print(tp)
+print(type(tp))
