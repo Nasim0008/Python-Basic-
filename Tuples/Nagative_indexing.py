77705377f6708b47f04564996c43e82885834661
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-16 15:55:27 +0600

    Negative indexing of the tuples

---
+++ Tuples/Nagative_indexing.py
@@ -0,0 +1,16 @@
+"""
+Negative indexing means start from the end
+-1 refers to the last index,
+-2 refers to the second last index
+........
+......
+-n refers to the first index
+"""
+
+tp = ("one","two","three","four","five")
+n = len(tp)
+
+print(tp[-1]) # last index item
+print(tp[-2]) # second last index item
+print(tp[-n]) # first index item
+
