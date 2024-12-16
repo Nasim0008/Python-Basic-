28e8d203c25f99d2b3488710c6011f3109dec880
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-16 16:05:22 +0600

    Access the item with negative indexing

---
+++ Tuples/Range_of_Negative_index.py
@@ -0,0 +1,12 @@
+
+"""
+Negative indexing means start from the end
+-1 refers to the last index,
+-2 refers to the second last index
+........
+......
+-n refers to the first index
+"""
+tp = (1,2,3,4,5,6)
+
+print(tp[-3:-1]) # print -3rd index (Included) to the -1 th index (Excluded)
