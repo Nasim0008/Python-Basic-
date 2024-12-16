017735a85ad69ef1f70d098f1fd65bfb8950f635
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-16 16:01:03 +0600

    access the item in range

---
+++ Tuples/Range_of_index.py
@@ -0,0 +1,11 @@
+tp  = ("one","two","three","four","five","six","seven")
+
+print(tp) # print all the item of tp
+
+print(tp[:]) # print all the item of tp
+
+print(tp[2:5]) #print 2nd index item to the 4th index item (5th index is not included)
+
+print(tp[3:]) # print from 3rd index item to the last index item
+
+print(tp[:5]) # print from the first item to the 5th index item
