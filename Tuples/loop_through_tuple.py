424d68866e94c3c59107cde021d25d45d8f3ae4c
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-19 11:43:03 +0600

    loop of tuple

---
+++ Tuples/loop_through_tuple.py
@@ -0,0 +1,21 @@
+tp =(1,2,3,4,5)
+
+# loop through the value
+for x in tp:
+    print(x,end=" ")
+
+print(end="\n")
+
+# loop through the index
+
+for i in range(len(tp)):
+    print(tp[i],end=" ")
+
+print(end="\n")
+
+i =0
+while i<len(tp):
+    print(tp[i],end=" ")
+    i+=1
+
+
