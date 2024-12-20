e59db97991a86a19bb8dd5d2584efe802e80e42d
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-20 14:54:44 +0600

    all data type of set

---
+++ Set/data_types.py
@@ -0,0 +1,12 @@
+se= {1,2,2,3,3,43,4,45,5,3} # int type
+print(se)
+
+se = {"one","two","there","four"} #str type
+print(se)
+
+se = {True,False,False,True} # boolean type
+print(se)
+
+se = {1,2,3,34,4,"one","two","there",False,True} # int, str, boolean types items
+print(se)
+
