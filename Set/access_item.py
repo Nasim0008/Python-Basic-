d296e4438b9414284a887d84d1e51185abcaef5a
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2024-12-20 15:00:57 +0600

    access the set items

---
+++ Set/access_item.py
@@ -0,0 +1,10 @@
+"""
+You cannot access items in a set by referring to an index or a key.
+But you can loop through the set items using a for loop,
+or ask if a specified value is present in a set, by using the in keyword.
+"""
+
+se = {1,1,23,3,4,4,False,"one","Nasim"}
+
+for x in se:
+    print(x,end = " ")
