ace78193b6061b14128ba1565fb5360379eb931d
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-06 12:37:58 +0600

    union of two set

---
+++ Set/Union_of_set.py
@@ -0,0 +1,14 @@
+# union means join two or multiples set
+
+s1 = {1,3,4,5,6}
+s2 = {"a","b","c","d"}
+s1=s1.union(s2) # s1+s2
+print(s1)
+print(s2)
+
+# we can | instead of union() function
+s1={1,2,3,4,5}
+s2={"a","b","c","d"}
+s1=s1|s2
+print(s1) #s1+s2
+print(s2)
