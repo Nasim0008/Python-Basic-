c736098648736e830c3954df95dc3627b7d87e86
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-17 11:51:51 +0600

    all data type

---
+++ Dictionary/data_type.py
@@ -0,0 +1,12 @@
+dic = {
+    "Name": "Md Nasim Hossen", #key:value
+    "Profession": "Software Engineer", #string
+     "age": 22, # int
+    "Marital status": False, #bool
+    "Salary": 1000000.000, #double
+    "Education Qualification": ["SSC","HSC"] # list
+}
+print(len(dic)) # number of items
+print(dic)
+
+print(type(dic))
