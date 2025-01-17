5c2ab54b26f50c74a5e3989605597e1c471002ce
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-17 11:45:47 +0600

    length of dictionary

---
+++ Dictionary/length.py
@@ -0,0 +1,7 @@
+dic = {
+    "Name": "Md Nasim Hossen", #key:value
+    "Father's Name": "Md Jalal Mollah",
+    "Mother Name": "Mst Nasima ",
+    "Profession": "Software Engineer"
+}
+print(len(dic)) # number of items
