5e423ec4ebfb819b732568bc4da9ee3e78e177c1
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-17 11:43:20 +0600

    all the properties of dictionary

---
+++ Dictionary/dic_properties.py
@@ -0,0 +1,22 @@
+# dictionary is ordered, changeable, and do not allow duplicated
+
+#Ordered:
+"""
+Dictionary is ordered, it means that, the items have a defined ordered, and this order will not change
+"""
+
+# Changeable
+"""
+Dictionaries are changeable, meaning that we can change, add or remove items after the dictionary has been created.
+"""
+
+#Duplicat Not allow
+"""
+Duplicate values will overwrite existing values
+"""
+
+dic = {
+    "Name": "XXX",
+    "Name": "Nasim" #overwrite
+}
+print(dic)
