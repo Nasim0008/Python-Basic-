f7a940862cfca76e7da8f668f6144c1c8e3ec4a9
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-17 11:32:34 +0600

    print the dictionary

---
+++ Dictionary/print_dic.py
@@ -0,0 +1,13 @@
+# dictionary are used to store the data in key:value in pairs
+# dictionary are written with carly braces and have key:value in pairs
+
+
+dic = {
+    "Name": "Md Nasim Hossen", #key:value
+    "Father's Name": "Md Jalal Mollah",
+    "Mother Name": "Mst Nasima ",
+    "Profession": "Software Engineer"
+}
+
+print(dic["Name"]) # print the value
+print(dic) # print the dictionary
