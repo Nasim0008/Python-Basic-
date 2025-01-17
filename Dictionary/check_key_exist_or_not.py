ee17e61347b9e92d4f922f70337724a9ffe96f04
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-17 12:13:39 +0600

    check the key is present or not in the dictionary

---
+++ Dictionary/check_key_exist_or_not.py
@@ -0,0 +1,13 @@
+dic = {
+    "Name": "Md Nasim Hossen", #key:value
+    "Profession": "Software Engineer", #string
+     "age": 22, # int
+    "Marital status": False, #bool
+    "Salary": 1000000.000, #double
+    "Education Qualification": ["SSC","HSC"] # list
+}
+
+if "Salary" in dic:
+    print("YES")
+else:
+    print("NO")
