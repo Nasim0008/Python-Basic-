8b2203c9fb1861e0f24d4a6fffb68ddfee1a1924
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-22 09:24:18 +0600

    copy one dictionary to another

---
+++ Dictionary/copy.py
@@ -0,0 +1,13 @@
+dic = {
+    "Name": "Md Nasim Hossen",
+    "Profession": "Software Engineer",
+     "Age": 22,
+     "Marital Status": False
+}
+
+# copy
+copy_dic = dic.copy() # copy() function
+print(copy_dic)
+
+copy02_dic = dict(dic) # dict() function
+print(copy02_dic)
