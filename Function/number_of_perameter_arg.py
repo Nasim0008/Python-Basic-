2fa18800c033ef68145f9b9b05183ce3b4e30417
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-27 21:02:59 +0600

    parameter and argument

---
+++ Function/number_of_perameter_arg.py
@@ -0,0 +1,17 @@
+#No need to declare the variable in parameter and argument
+
+def My_function(): # No parameter
+    print("Md Nasim Hossen")
+
+My_function()
+
+def add(a,b): # Two parameter
+    return a+b
+
+print(add(10,23))
+
+
+def add(a,b,c): # three parameter
+    return a+b+c
+print(add(10,20,30))
+
