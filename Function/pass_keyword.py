46dc1459c1dcf06d1e56f548bcdc83c13c84da36
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2025-01-27 21:49:43 +0600

    pass keyword in function

---
+++ Function/pass_keyword.py
@@ -0,0 +1,11 @@
+
+"""
+function definitions cannot be empty,
+but if you for some reason have a function definition with no content,
+put in the pass statement to avoid getting an error.
+"""
+
+def function():
+    pass
+
+print(function())
